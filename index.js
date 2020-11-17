e7f441178d462b0338f8cd4eccbb3c018c762f51
nandopeyton <68226901+nandopeyton@users.noreply.github.com>
2020-11-17 14:48:54 -0300

    fixed import of useradminRoutes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const productRoutes = require("./routes/productRoutes");
 const categoriesRoutes = require("./routes/categoriesRoutes");
 const userRoutes = require("./routes/userRoutes");
 const orderRoutes = require("./routes/orderRoutes");
-
+const userAdminRoutes = require("./routes/userAdminRoutes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,9 +16,7 @@ productRoutes(app);
 categoriesRoutes(app);
 userRoutes(app);
 orderRoutes(app);
-
-
-
+userAdminRoutes(app);
 
 app.listen(process.env.APP_PORT, () => {
   console.log("ingresar a " + process.env.APP_PORT);
